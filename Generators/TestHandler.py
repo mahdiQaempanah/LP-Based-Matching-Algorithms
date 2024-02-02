4e8dd16365b4b2a0c8f53058b9e6f3819ae8d451
Mahdi <mahdi13812002@gmail.com>
2024-02-02 22:28:21 +0330

    add casual tests

--- Generators/TestHandler.py
+++ Generators/TestHandler.py
@@ -2,6 +2,7 @@ import os
 import random 
 import math 
 from enum import Enum
+import sys 
 
 class TestType(Enum):
     CS = 1
@@ -13,9 +14,10 @@ class TestType(Enum):
 
 class TestGenerator:
     
-    tests_path = {TestType.CS: '../Tests/cs/',
-                  TestType.CD: '../Tests/cs/', 
+    tests_path = {
                   TestType.WSL: '../Tests/wsl/',
+                  TestType.CS: '../Tests/cs/',
+                  TestType.CD: '../Tests/cd/', 
                   TestType.WSH: '../Tests/wsh/',
                   TestType.WDL: '../Tests/wdl/',
                   TestType.WDH: '../Tests/wdh/'}
@@ -25,12 +27,12 @@ class TestGenerator:
         pass 
 
     @staticmethod
-    def generate(test_type, t):
+    def generate(test_type, t, casual_test ):
         print(f'making {t} test for {test_type}:')
         base = [10, 100, 500, 1000, 10000]
         n1s = [random.randint(base[i-1]//2, base[i-1]+(base[i-1]//2)) for i in range(1, t+1)]
         n2s = [random.randint(base[i-1]//2, base[i-1]+(base[i-1]//2)) for i in range(1, t+1)]
-        n1s[2] = n2s[2] = 500
+
 
         edges = [] 
         for test_id in range(1, t+1):
@@ -56,6 +58,32 @@ class TestGenerator:
             print(f'test number {test_id} added succesfully.')
         print()
 
+        n1s = [random.randint(100, 200) for i in range(1, casual_test+1)]
+        n2s = [random.randint(100, 200) for i in range(1, casual_test+1)]
+        edges = [] 
+        for test_id in range(1, casual_test+1):
+            signle_edges = []
+            match test_type:
+                case TestType.CS:
+                    signle_edges = TestGenerator.cs(n1s[test_id-1], n2s[test_id-1])
+                case TestType.CD:
+                    signle_edges = TestGenerator.cd(n1s[test_id-1], n2s[test_id-1])
+                case TestType.WSL:
+                    signle_edges = TestGenerator.wsl(n1s[test_id-1], n2s[test_id-1])
+                case TestType.WSH:
+                    signle_edges = TestGenerator.wsh(n1s[test_id-1], n2s[test_id-1])
+                case TestType.WDL:
+                    signle_edges = TestGenerator.wdl(n1s[test_id-1], n2s[test_id-1])
+                case TestType.WDH:
+                    signle_edges = TestGenerator.wdh(n1s[test_id-1], n2s[test_id-1])
+                case _:
+                    return Exception(f"invalid test type:{test_type}")
+            edges.append(signle_edges)
+        for test_id in range(1, casual_test+1):
+            TestGenerator.write_test(f'{TestGenerator.tests_path[test_type]}casual_input{test_id}.txt', n1s[test_id-1], n2s[test_id-1], edges[test_id-1])
+            print(f'casual test number {test_id} added succesfully.')
+        print()
+
     @staticmethod
     def write_test(path, n1, n2, edges):
         with open(path, 'w') as f:
@@ -65,15 +93,15 @@ class TestGenerator:
 
     @staticmethod
     def sparse(n1, n2):
-        m_factor = random.randint(10, 30)
-        m = int(min(m_factor*(n1+n2), n1*n2, 1e5))
+        m_factor = random.random()*3 + 1
+        m = int(min(m_factor*(min(n1,n2)), (n1*n2)/10, 1e5))
         edge_tmp = random.sample(range(0, n1*n2), m)
-        edge = [(i//n1, (i%n2)+n1) for i in edge_tmp]
+        edge = [(i//n2, (i%n2)+n1) for i in edge_tmp]
         return edge 
     
     @staticmethod
     def dense(n1, n2):
-        m_factor = int(math.sqrt(n1+n2)) * random.randint(1, 15)
+        m_factor = math.sqrt(min(n1,n2)) * (random.random()*5+1)
         m = int(min(m_factor*(n1+n2), n1*n2, 1e5))
         edge_tmp = random.sample(range(0, n1*n2), m)
         edge = [(i//n2, (i%n2)+n1) for i in edge_tmp]
@@ -81,12 +109,12 @@ class TestGenerator:
 
     @staticmethod
     def low_weight(n1, n2, edges):
-        answer = [(i, j, random.randint(0, len(edges)*5)) for (i,j) in edges]
+        answer = [(i, j, random.randint(0, 100)+1) for (i,j) in edges]
         return answer 
     
     @staticmethod
     def high_weight(n1, n2, edges):
-        answer = [(i, j, random.randint(0, 1e9)+1) for (i,j) in edges]
+        answer = [(i, j, random.randint(0, 100000)+1) for (i,j) in edges]
         return answer 
 
     @staticmethod
@@ -139,11 +167,28 @@ class TestGenerator:
                 edges.append((i, j, w))
             test = {'n1':n1, 'n2':n2, 'm':m, 'n':n1+n2, 'edges':edges}
         return test
+    
+    @staticmethod
+    def get_casual_test(test_type, test_id):
+        test = {}
+        with open(f'{TestGenerator.tests_path[test_type]}/casual_input{test_id}.txt', 'r') as f:
+            txt = f.read()
+            txt_split = txt.split('\n')
+            n1n2m , edges_str = txt_split[0], txt_split[1:]
+            n1, n2, m = map(int, n1n2m.split())
+            edges = []
+            for s in edges_str:
+                i, j, w = map(int, s.split())
+                edges.append((i, j, w))
+            test = {'n1':n1, 'n2':n2, 'm':m, 'n':n1+n2, 'edges':edges}
+        return test
 
 
-# t = 5
-# for test_type in TestType:
-#     TestGenerator.generate(test_type, t)
+if len(sys.argv) > 1 and sys.argv[1] == 'mt':
+    t = 5
+    casual_test = 15 
+    for test_type in TestType:
+        TestGenerator.generate(test_type, t, casual_test)
 
 
 
--- Tests/ValidityTest.py
+++ Tests/ValidityTest.py
@@ -6,12 +6,15 @@ from MatchingLP import MatchingLP
 from TestHandler import TestGenerator, TestType
 from MultiplicativeAuction import MultiplicativeAuction
 
-for i in range(1, 4):
-    test = TestGenerator.get_test(TestType.CS, i)
-    matchingLP = MatchingLP(test['n'], test['edges'])
-    hungarianAlgorithm = HungarianAlgorithm(test['n1'], test['n2'], test['edges'])
-    multiplicativeAuction = MultiplicativeAuction(test['n1'], test['n2'], test['edges'])
-    print(f'test {i}:')
-    print(f'hungarian algorithm answer: {hungarianAlgorithm.solve()}')
-    print(f'lp answer: {matchingLP.solve()}')
-    print(f'multiplicativeAuction algorithm answer: {multiplicativeAuction.solve(0.1)}')
+
+for test_type in TestType:
+    print(f'testing {test_type}:')
+    for i in range(1, 2):
+        test = TestGenerator.get_casual_test(test_type, i)
+        matchingLP = MatchingLP(test['n'], test['edges'])
+        hungarianAlgorithm = HungarianAlgorithm(test['n1'], test['n2'], test['edges'])
+        multiplicativeAuction = MultiplicativeAuction(test['n1'], test['n2'], test['edges'])
+        print(f'test {i}:')
+        print(f'hungarian answer: {hungarianAlgorithm.solve()} ', end='')
+        print(f'lp answer: {matchingLP.solve()}', end=' ')
+        print(f'multiplicativeAuction answer: {multiplicativeAuction.solve(0.999)}')
